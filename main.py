dcfa3e5e22c94404946cb4d63c852cf1779ca648
kalismile <kalismile@outlook.com>
2019-09-01 22:25:18 +0800

    add file

--- main.py
+++ main.py
@@ -1,51 +1,97 @@
-from urllib.parse import quote
-import urllib3
 import certifi
 import json
 import time
-import math 
+import math
 import random
 import os
+import requests
 # import tkinter as tk
 
-    
-def search_user_sec_user_id(keyword):
-    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
-    keyword = quote(keyword, 'utf-8')
+
+def search_user_sec_user_id(user_name):
+
     ticks = time.time()
     new_ticks = str(ticks).replace('.', '')
-    ts=new_ticks[0:10]
-    rticket=new_ticks[0:13]
-    device_id=random.random()
-    device_id=math.ceil(device_id*100000000000)
-    device_id=str(device_id)
-    url='https://aweme-hl.snssdk.com/aweme/v1/general/search/single/?manifest_version_code=730&_rticket='+rticket+'&app_type=normal&iid=80593983692&channel=huawei&device_type=HWI-AL00&language=zh&resolution=1080*2160&openudid=dd9979d88efc3c7e&update_version_code=7302&os_api=26&dpi=408&ac=wifi&device_id='+device_id+'&mcc_mnc=46003&os_version=8.0.0&version_code=730&app_name=aweme&version_name=7.3.0&js_sdk_version=1.19.2.0&device_brand=HUAWEI&ssmix=a&device_platform=android&aid=1128&ts='+ts+'&keyword='+keyword+'&offset=0&count=10&is_pull_refresh=0&search_source=normal_search&hot_search=0&latitude=31.992811&longitude=112.120394&search_id=&query_correct_type=1'
-    r=http.request('GET',url)
+    proxies = {
+        'https':'http://192.168.124.9:8888',
+        'http':'http://192.168.124.9:8888',
+    }
+    params = {
+        'os_api': '22',
+        'device_type': 'm2',
+        'ssmix': 'a',
+        'manifest_version_code': '720',
+        'dpi': '240',
+        'js_sdk_version': '1.19.4.16',
+        'uuid': '865166026335151',
+        'app_name': 'aweme',
+        'version_name': '7.2.0',
+        'ts': str(new_ticks[0:10]),
+        'app_type': 'normal',
+        'ac': 'wifi',
+        'update_version_code': '7202',
+        'channel': 'aweGW',
+        '_rticket': str(new_ticks[0:13]),
+        'device_platform': 'android',
+        'iid': '84609604981',
+        'version_code': '720',
+        'openudid': '926bb1e95c3d4e10',
+        'device_id': '66662367106',
+        'resolution': '1080*1920',
+        'os_version': '5.1.1',
+        'language': 'zh',
+        'device_brand': 'meizu',
+        'aid': '1128',
+        'mcc_mnc': '46000',
+    }
+    data = {
+        #  body
+
+        'cursor': '0',
+        'keyword': '051YP',
+        'count': '10',
+        'type': '1',
+        'is_pull_refresh': '0',
+        'hot_search': '0',
+        'search_source': '',
+        'search_id': '',
+        'query_correct_type': '1',
+    }
+
+    r = requests.post(
+        'https://aweme-hl.snssdk.com/aweme/v1/general/search/single/?',
+        params=params,
+        data=data,
+        verify=certifi.where(),
+        proxies=proxies
+    )
     return json.loads(r.data.decode('utf-8'))
 
 
 def search_user_vedio_list(search_user):
-    sec_user_id=search_user['data'][0]['user_list'][0]['user_info']['sec_uid']
+    sec_user_id = search_user['data'][0]['user_list'][0]['user_info']['sec_uid']
     # aweme_count=search_user['data'][0]['user_list'][0]['user_info']['aweme_count']
-    vedio_list=[]
+    vedio_list = []
 
-    max_cursor=0
+    max_cursor = 0
     while(True):
-    # for i in range(2):
+        # for i in range(2):
         ticks = time.time()
         new_ticks = str(ticks).replace('.', '')
-        ts=new_ticks[0:10]
-        rticket=new_ticks[0:13]
+        ts = new_ticks[0:10]
+        rticket = new_ticks[0:13]
 
-        url='https://api-hl.amemv.com/aweme/v1/aweme/post/?max_cursor='+str(max_cursor)+'&sec_user_id='+sec_user_id+'&count=10&retry_type=no_retry&iid=80593983692&device_id=58231544547&ac=wifi&channel=huawei&aid=1128&app_name=aweme&version_code=730&version_name=7.3.0&device_platform=android&ssmix=a&device_type=HWI-AL00&device_brand=HUAWEI&language=zh&os_api=26&os_version=8.0.0&openudid=dd9979d88efc3c7e&manifest_version_code=730&resolution=1080*2160&dpi=408&update_version_code=7302&_rticket='+rticket+'&app_type=normal&js_sdk_version=1.19.2.0&mcc_mnc=46003&ts='+ts
-        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
-        r=http.request('GET',url)
-        r=r.data
-        r=json.loads(r)
-        max_cursor=r['max_cursor']
+        url = 'https://api-hl.amemv.com/aweme/v1/aweme/post/?max_cursor=' + \
+            str(max_cursor)+'&sec_user_id='+sec_user_id+'&count=10&retry_type=no_retry&iid=80593983692&device_id=58231544547&ac=wifi&channel=huawei&aid=1128&app_name=aweme&version_code=730&version_name=7.3.0&device_platform=android&ssmix=a&device_type=HWI-AL00&device_brand=HUAWEI&language=zh&os_api=26&os_version=8.0.0&openudid=dd9979d88efc3c7e&manifest_version_code=730&resolution=1080*2160&dpi=408&update_version_code=7302&_rticket='+rticket+'&app_type=normal&js_sdk_version=1.19.2.0&mcc_mnc=46003&ts='+ts
+        http = urllib3.PoolManager(
+            cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+        r = http.request('GET', url)
+        r = r.data
+        r = json.loads(r)
+        max_cursor = r['max_cursor']
         for j in range(len(r['aweme_list'])):
             try:
-                download_url=r['aweme_list'][j]['video']['bit_rate'][0]['play_addr']['url_list'][0]
+                download_url = r['aweme_list'][j]['video']['bit_rate'][0]['play_addr']['url_list'][0]
                 vedio_list.append(download_url)
             except:
                 pass
@@ -56,44 +102,45 @@ def search_user_vedio_list(search_user):
         if not int(r['has_more']):
             break
     # 去重
-    vedio_list_alone=list(set(vedio_list))    
+    vedio_list_alone = list(set(vedio_list))
     # 文件写入
-    f=open('download_url.txt','a')
+    f = open('download_url.txt', 'a')
     for i in range(len(vedio_list_alone)):
         f.write(vedio_list_alone[i])
         f.write('\n')
     f.close()
     return vedio_list_alone
 
-def download_vedio(vedio_list,folder):
+
+def download_vedio(vedio_list, folder):
     for i in range(len(vedio_list)):
-        url=vedio_list[i]
-        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
-        r=http.request('GET',url)
-        data=r.data
+        url = vedio_list[i]
+        http = urllib3.PoolManager(
+            cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+        r = http.request('GET', url)
+        data = r.data
 
         if os.path.exists(folder):
             pass
         else:
             os.mkdir(folder)
 
-        f=open(folder+'/'+folder+'-'+str(i)+'.mp4','wb')
+        f = open(folder+'/'+folder+'-'+str(i)+'.mp4', 'wb')
         f.write(data)
         f.close()
         time.sleep(5)
 
 
 def input_user_name(user_name):
-    search_user=search_user_sec_user_id(user_name)
-    #'zh19980123'
-    vedio_list=search_user_vedio_list(search_user)
-    download_vedio(vedio_list,user_name)
+    search_user = search_user_sec_user_id(user_name)
+    # 'zh19980123'
+    vedio_list = search_user_vedio_list(search_user)
+    download_vedio(vedio_list, user_name)
+
 
 if __name__ == "__main__":
 
-    input_user_name('2227656401')   
-
-
+    input_user_name('051YP')
 
 
 # # top = tk.Tk()
@@ -109,7 +156,7 @@ if __name__ == "__main__":
 #     vedio_list=search_user_vedio_list(search_user)
 #     download_vedio(vedio_list)
 #     print(vedio_list)
-    
+
 
 # bt = tk.Button(top, text="Download", command=task)  # 按钮
 
@@ -117,9 +164,3 @@ if __name__ == "__main__":
 # bt.pack()    # 将小部件放置到主窗口中
 
 # top.mainloop()   # 进入消息循环
-
-
-
-
-
-
---
+++ test.py
@@ -0,0 +1,71 @@
+import certifi
+import json
+import time
+import math
+import random
+import os
+import requests
+
+
+def search_user_sec_user_id(user_name):
+
+    ticks = time.time()
+    new_ticks = str(ticks).replace('.', '')
+    proxies = {
+        'https':'http://127.0.0.1:8888',
+        'http':'http://127.0.0.1:8888',
+    }
+    # params = {
+    #     'os_api': '22',
+    #     'device_type': 'm2',
+    #     'ssmix': 'a',
+    #     'manifest_version_code': '720',
+    #     'dpi': '240',
+    #     'js_sdk_version': '1.19.4.16',
+    #     'uuid': '865166026335151',
+    #     'app_name': 'aweme',
+    #     'version_name': '7.2.0',
+    #     'ts': str(new_ticks[0:10]),
+    #     'app_type': 'normal',
+    #     'ac': 'wifi',
+    #     'update_version_code': '7202',
+    #     'channel': 'aweGW',
+    #     '_rticket': str(new_ticks[0:13]),
+    #     'device_platform': 'android',
+    #     'iid': '84609604981',
+    #     'version_code': '720',
+    #     'openudid': '926bb1e95c3d4e10',
+    #     'device_id': '66662367106',
+    #     'resolution': '1080*1920',
+    #     'os_version': '5.1.1',
+    #     'language': 'zh',
+    #     'device_brand': 'meizu',
+    #     'aid': '1128',
+    #     'mcc_mnc': '46000',
+    # }
+    # data = {
+    #     #  body
+
+    #     'cursor': '0',
+    #     'keyword': '051YP',
+    #     'count': '10',
+    #     'type': '1',
+    #     'is_pull_refresh': '0',
+    #     'hot_search': '0',
+    #     'search_source': '',
+    #     'search_id': '',
+    #     'query_correct_type': '1',
+    # }
+
+    # r = requests.post(
+    #     'https://aweme-hl.snssdk.com/aweme/v1/general/search/single/?',
+    #     # params=params,
+    #     data=data,
+    #     verify='e:/',
+    #     proxies=proxies
+    # )
+    print(certifi.where())
+    r=requests.get('http://www.heao.gov.cn/',proxies=proxies)
+    return r.text
+if __name__ == "__main__":
+    print(search_user_sec_user_id('051YP'))
